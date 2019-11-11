f149fbf72538480da5ee46fd81bad38656487048
thisleejones <thisleejones@gmail.com>
2019-11-11 09:42:04 +0000

    patch/keyboard_select-20190207

--- config.h
+++ config.h
@@ -256,6 +256,7 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
 	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
 	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
+	{ TERMMOD,              XK_Escape,      keyboard_select,{ 0 } },
 };
 
 /*
